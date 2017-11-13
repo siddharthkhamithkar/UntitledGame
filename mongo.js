d9ed739be96b21f789ee6235045579b152b08369
Siddharth Khamithkar <sidkhamithkar@gmail.com>
2017-11-13 16:17:34 +0530

    New File

---
+++ mongo.js
@@ -0,0 +1,13 @@
+db.test1.insert({
+  "My Name" : "Siddharth",
+  "My Age" : NumberLong(14),
+  "My Interests" : {
+    "Game" : "RuneScape",
+    "Food" : "Chicken?",
+    "Book" : "Fantasy",
+    "Anime" : "Fairy Tail",
+  },
+  "Why?" : "I'm interested",
+  "how?" : "My brother suggested me MongoDB",
+  "When?" : "A few weeks ago"
+})
